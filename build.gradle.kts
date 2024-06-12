dbc97bc4cd9639ca2d092e9f07c1be2fd05a963e
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-06-13 01:26:00 +0200

    chore: add lombok global dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,9 @@ allprojects {
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
         testImplementation("org.junit.jupiter:junit-jupiter")
+
+        compileOnly("org.projectlombok:lombok:1.18.32")
+        annotationProcessor("org.projectlombok:lombok:1.18.32")
     }
 
     tasks.test {
