8d3259384b0d686fda40226e8dc5648fa732e0f2
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-06-13 02:14:54 +0200

    feat(compiler): add token metadata

---
+++ compiler/src/main/java/org/voidlang/compiler/token/TokenMeta.java
@@ -0,0 +1,14 @@
+package org.voidlang.compiler.token;
+
+/**
+ * Represents the metadata of a parsed token, that helps to identify the token's position in the source code.
+ * <p>
+ * This ensures, that we can provide better error messages and debugging information.
+ *
+ * @param beginIndex the beginning index of the token
+ * @param endIndex the ending index of the token
+ * @param lineIndex the index of the first character in the line of the token being processed
+ * @param lineNumber the number of the current line being processed for the token
+ */
+public record TokenMeta(int beginIndex, int endIndex, int lineIndex, int lineNumber) {
+}
