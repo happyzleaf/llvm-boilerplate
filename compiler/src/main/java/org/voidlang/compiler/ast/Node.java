b299679a598534ffdd19a4aee1e043354431a89b
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-06-13 19:45:11 +0200

    feat(compiler): add ast node

---
+++ compiler/src/main/java/org/voidlang/compiler/ast/Node.java
@@ -0,0 +1,32 @@
+package org.voidlang.compiler.ast;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents the base class for all element of the Abstract Syntax Tree.
+ * <p>
+ * It is used to represent the structure of the code in a tree-like structure.
+ * The tree is composed of nodes, where each node represents a single element of the code.
+ * <p>
+ * For example the code {@code 1 + (2 - 3)} can be represented as {@code ADD(1, SUB(2, 3))}.
+ */
+@Accessors(fluent = true)
+@Getter
+public abstract class Node {
+    /**
+     * The type of the node.
+     */
+    private final @NotNull NodeType nodeType;
+
+    /**
+     * Initialize the node and retrieve the type from the annotation.
+     */
+    public Node() {
+        NodeInfo info = getClass().getAnnotation(NodeInfo.class);
+        if (info == null)
+            throw new IllegalStateException(getClass().getSimpleName() + " does not have @NodeInfo");
+        nodeType = info.type();
+    }
+}
---
+++ compiler/src/main/java/org/voidlang/compiler/ast/NodeType.java
@@ -0,0 +1,7 @@
+package org.voidlang.compiler.ast;
+
+/**
+ * Represents an enumeration of the types of nodes that can be present in the AST.
+ */
+public enum NodeType {
+}
