b8ab5c9faaef981d9c1d814772a75d4f756f94e3
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-06-15 17:31:24 +0200

    feat(compiler): add scalar anonymous type

---
+++ compiler/src/main/java/org/voidlang/compiler/ast/type/anonymous/ScalarType.java
@@ -0,0 +1,20 @@
+package org.voidlang.compiler.ast.type.anonymous;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.voidlang.compiler.ast.type.array.Array;
+import org.voidlang.compiler.ast.type.name.TypeName;
+import org.voidlang.compiler.ast.type.referencing.Referencing;
+
+/**
+ * Represents the most primitive type in the Abstract Syntax Tree, which only holds a single type, therefore is
+ * called a scalar type.
+ *
+ * @param referencing the indication on how the type should be treated as
+ * @param array the array specifier of the type
+ * @param memberName the name of the member, if this type is a member of a {@link TupleType}
+ */
+public record ScalarType(
+    @NotNull Referencing referencing, @NotNull TypeName name, @NotNull Array array, @Nullable String memberName
+) implements AnonymousType {
+}
