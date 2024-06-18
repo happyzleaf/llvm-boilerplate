f7ad11d56df65d2dc25f65ea68fe6d44e142b830
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-06-18 16:36:50 +0200

    feat(llvm-wrapper): add module verification

--- llvm-wrapper/src/main/java/org/voidlang/llvm/module/IRModule.java
+++ llvm-wrapper/src/main/java/org/voidlang/llvm/module/IRModule.java
@@ -1,10 +1,12 @@
 package org.voidlang.llvm.module;
 
+import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.llvm.LLVM.LLVMModuleRef;
 import static org.bytedeco.llvm.global.LLVM.*;
 
 import org.jetbrains.annotations.NotNull;
 import org.voidlang.llvm.behaviour.Disposable;
+import org.voidlang.llvm.error.VerificationFailureAction;
 
 /**
  * Represents an LLVM module that is associated with a source file.
@@ -29,6 +31,18 @@ public record IRModule(
      */
     public void dump() {
         LLVMDumpModule(handle);
+    }
+
+    /**
+     * Verify the module. Perform the specified {@param action} if the verification fails and store the error
+     * message in the {@param error} buffer.
+     *
+     * @param action the action to take when the verification fails
+     * @param error the error message buffer
+     * @return {@code true} if the verification was successful, otherwise {@code false}
+     */
+    public boolean verify(@NotNull VerificationFailureAction action, @NotNull BytePointer error) {
+        return LLVMVerifyModule(handle, action.code(), error) == 0;
     }
 
     /**
